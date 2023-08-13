d0bbb6fb8fe5a63ec187e99fd48a1a4c314a041c
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-08-13 15:47:41 +0530

    Create TwofishAdv.cpp

---
+++ Encryptions/TwofishAdv.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cryptopp/twofish.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/filters.h>
+
+int main() {
+    try {
+        CryptoPP::Twofish::Encryption twofishEncryption;
+        CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption(twofishEncryption, new CryptoPP::StreamTransformationFilter::BlockPaddingSchemeDef::PKCS_PADDING);
+        byte key[CryptoPP::Twofish::DEFAULT_KEYLENGTH];
+        byte iv[CryptoPP::Twofish::BLOCKSIZE];
+        std::memset(key, 0x00, CryptoPP::Twofish::DEFAULT_KEYLENGTH);
+        std::memset(iv, 0x00, CryptoPP::Twofish::BLOCKSIZE);
+
+        twofishEncryption.SetKeyWithIV(key, CryptoPP::Twofish::DEFAULT_KEYLENGTH, iv);
+        std::string plaintext = "Hello, Twofish!";
+        std::string ciphertext;
+        CryptoPP::StringSource(plaintext, true,
+            new CryptoPP::StreamTransformationFilter(cbcEncryption,
+                new CryptoPP::StringSink(ciphertext)
+            )
+        );
+
+        std::cout << "Encrypted: " << ciphertext << std::endl;
+        CryptoPP::Twofish::Decryption twofishDecryption;
+        CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption(twofishDecryption, new CryptoPP::StreamTransformationFilter::BlockPaddingSchemeDef::PKCS_PADDING);
+        twofishDecryption.SetKeyWithIV(key, CryptoPP::Twofish::DEFAULT_KEYLENGTH, iv);
+        std::string decrypted;
+        CryptoPP::StringSource(ciphertext, true,
+            new CryptoPP::StreamTransformationFilter(cbcDecryption,
+                new CryptoPP::StringSink(decrypted)
+            )
+        );
+
+        std::cout << "Decrypted: " << decrypted << std::endl;
+    } catch (const CryptoPP::Exception &ex) {
+        std::cerr << "Crypto++ Exception: " << ex.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
