7fce121506a4510b029d3b6612b92fa4b4b7a955
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-12-17 21:55:09 +0530

    Create checkAES.cpp

---
+++ checkAES.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+bool isFileEncryptedWithAES(const std::string& filePath) {
+    std::ifstream file(filePath, std::ios::binary);
+    if (file) {
+        char header[16];
+        file.read(header, sizeof(header));
+        //for AES, it could be something like checking for "AES-128" or "AES-256"
+        bool isAESSignature = /*check if header matches AES signature*/;
+        
+        file.close();
+        return isAESSignature;
+    }
+    return false;
+}
+
+int main() {
+    std::string filePath = "example.txt";
+    if (isFileEncryptedWithAES(filePath)) {
+        std::cout << "File is encrypted with AES.\n";
+    } else {
+        std::cout << "File is not encrypted with AES.\n";
+    }
+    return 0;
+}
